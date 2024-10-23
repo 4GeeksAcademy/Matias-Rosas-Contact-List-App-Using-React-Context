4f0b8aaefceed220d01b044bf6e5f779732eaec9
MatiRosas31 <165863634+MatiRosas31@users.noreply.github.com>
2024-10-23 00:26:51 +0000

    [Blank contacts prevent feature added] -Se agrego una funcion para evitar que se crearan contactos con informacion vacia -

--- src/js/views/NuevoContacto.js
+++ src/js/views/NuevoContacto.js
@@ -1,21 +1,28 @@
 import React, { useState } from 'react'
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const NuevoContacto = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [name, setName] = useState("");
-  const [email, setEmail] =useState("");
-  const [phone, setPhone] =useState("");
-  const [address, setAddress] =useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [error, setError] = useState("");
 
-  function addNewContact (name,email,phone, address) {
-    if (name !== "" && email !== "" && phone !== "" && address !== "") {
+  function addNewContact(name, email, phone, address) {
+    if (name === "" || email === "" || phone === "" || address === "") {
+      setError("All fields are required!"); 
+    }
+
+    setError("");
+
     let newContactobj = {
       name: name,
       phone: phone,
       email: email,
       address: address
-    }
+    };
+
     fetch("https://playground.4geeks.com/contact/agendas/MatiRosas31/contacts", {
       method: "POST",
       body: JSON.stringify(newContactobj),
@@ -31,41 +38,46 @@ const NuevoContacto = () => {
     .then((contact) => {
       console.log("Nuevo contacto creado: ", contact);
     })
-    .catch((error) => console.log(error));  
-    }
+    .catch((error) => console.log(error));
+
+    setName("");
+    setEmail("");
+    setPhone("");
+    setAddress("");
   }
 
-    return (
-        <>
-        <div className="container">
-      <h1 className="text-center mt-5">Add a new contact</h1>
-      <form className="mt-4">
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">Full Name</label>
-          <input type="text" className="form-control" id="name" placeholder="Full Name" value={name}  onChange={(e) => setName(e.target.value)} />
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-center mt-5">Add a new contact</h1>
+        <form className="mt-4">
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">Full Name</label>
+            <input type="text" className="form-control" id="name" placeholder="Full Name" value={name} onChange={(e) => setName(e.target.value)} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email</label>
+            <input type="email" className="form-control" id="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="phone" className="form-label">Phone</label>
+            <input type="tel" className="form-control" id="phone" placeholder="Enter phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="address" className="form-label">Address</label>
+            <input type="text" className="form-control" id="address" placeholder="Enter address" value={address} onChange={(e) => setAddress(e.target.value)} />
+          </div>
+          {error !== "" ? <p className="text-danger">{error}</p> : ""}
+          <button type="button" className="btn btn-primary w-100" onClick={() => addNewContact(name, email, phone, address)}>Save</button>
+        </form>
+        <div className="text-center mt-3">
+          <a className="text-muted user-select-auto" onClick={() => {
+            navigate("/");
+          }}>or get back to contacts</a>
         </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email</label>
-          <input type="email" className="form-control" id="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="phone" className="form-label">Phone</label>
-          <input type="tel" className="form-control" id="phone" placeholder="Enter phone" value={phone} onChange={(e) => setPhone(e.target.value)}/>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="address" className="form-label">Address</label>
-          <input type="text" className="form-control" id="address" placeholder="Enter address" value={address} onChange={(e) => setAddress(e.target.value)}/>
-        </div>
-        <button type="button" className="btn btn-primary w-100" onClick={()=> {addNewContact(name, email, phone, address), setName(""), setEmail(""), setPhone(""), setAddress("")}}>save</button>
-      </form>
-      <div className="text-center mt-3">
-        <a className="text-muted user-select-auto" onClick={()=>{
-          navigate("/")
-        }}>or get back to contacts</a>
       </div>
-    </div>
-        </>
-    )
-}
+    </>
+  );
+};
 
 export default NuevoContacto;
